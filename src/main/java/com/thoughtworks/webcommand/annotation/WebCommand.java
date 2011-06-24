bba14b9092dd2b3af6f52ffae95c423d360b6430
fanliu <sharprazor@gmail.com>
2011-06-24 16:19:08 +0800

    wrap creation of command handler invoker into locator

--- src/main/java/com/thoughtworks/webcommand/annotation/WebCommand.java
+++ src/main/java/com/thoughtworks/webcommand/annotation/WebCommand.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RequestMapping {
+public @interface WebCommand {
 
     String uri() default "/";
     HttpVerb verb() default HttpVerb.GET;
